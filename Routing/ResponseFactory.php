cf5508a2188950bcd78ac7bf591f34a482c7b191
HanniBunny <hnnhcas@gmail.com>
2025-05-18 11:11:48 +0800

    Add files via upload

--- Routing/ResponseFactory.php
+++ Routing/ResponseFactory.php
@@ -1,18 +1,64 @@
 <?php
 
-namespace Illuminate\Contracts\Routing;
+namespace Illuminate\Routing;
 
-interface ResponseFactory
+use Closure;
+use Illuminate\Contracts\Routing\ResponseFactory as FactoryContract;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Http\StreamedEvent;
+use Illuminate\Routing\Exceptions\StreamedResponseException;
+use Illuminate\Support\Js;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\StreamedJsonResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Throwable;
+
+class ResponseFactory implements FactoryContract
 {
+    use Macroable;
+
+    /**
+     * The view factory instance.
+     *
+     * @var \Illuminate\Contracts\View\Factory
+     */
+    protected $view;
+
+    /**
+     * The redirector instance.
+     *
+     * @var \Illuminate\Routing\Redirector
+     */
+    protected $redirector;
+
+    /**
+     * Create a new response factory instance.
+     *
+     * @param  \Illuminate\Contracts\View\Factory  $view
+     * @param  \Illuminate\Routing\Redirector  $redirector
+     */
+    public function __construct(ViewFactory $view, Redirector $redirector)
+    {
+        $this->view = $view;
+        $this->redirector = $redirector;
+    }
+
     /**
      * Create a new response instance.
      *
-     * @param  array|string  $content
+     * @param  mixed  $content
      * @param  int  $status
      * @param  array  $headers
      * @return \Illuminate\Http\Response
      */
-    public function make($content = '', $status = 200, array $headers = []);
+    public function make($content = '', $status = 200, array $headers = [])
+    {
+        return new Response($content, $status, $headers);
+    }
 
     /**
      * Create a new "no content" response.
@@ -21,7 +67,10 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Illuminate\Http\Response
      */
-    public function noContent($status = 204, array $headers = []);
+    public function noContent($status = 204, array $headers = [])
+    {
+        return $this->make('', $status, $headers);
+    }
 
     /**
      * Create a new response for a given view.
@@ -32,7 +81,14 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Illuminate\Http\Response
      */
-    public function view($view, $data = [], $status = 200, array $headers = []);
+    public function view($view, $data = [], $status = 200, array $headers = [])
+    {
+        if (is_array($view)) {
+            return $this->make($this->view->first($view, $data), $status, $headers);
+        }
+
+        return $this->make($this->view->make($view, $data), $status, $headers);
+    }
 
     /**
      * Create a new JSON response instance.
@@ -43,7 +99,10 @@ interface ResponseFactory
      * @param  int  $options
      * @return \Illuminate\Http\JsonResponse
      */
-    public function json($data = [], $status = 200, array $headers = [], $options = 0);
+    public function json($data = [], $status = 200, array $headers = [], $options = 0)
+    {
+        return new JsonResponse($data, $status, $headers, $options);
+    }
 
     /**
      * Create a new JSONP response instance.
@@ -55,7 +114,73 @@ interface ResponseFactory
      * @param  int  $options
      * @return \Illuminate\Http\JsonResponse
      */
-    public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0);
+    public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
+    {
+        return $this->json($data, $status, $headers, $options)->setCallback($callback);
+    }
+
+    /**
+     * Create a new event stream response.
+     *
+     * @param  \Closure  $callback
+     * @param  array  $headers
+     * @param  \Illuminate\Http\StreamedEvent|string|null  $endStreamWith
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function eventStream(Closure $callback, array $headers = [], StreamedEvent|string|null $endStreamWith = '</stream>')
+    {
+        return $this->stream(function () use ($callback, $endStreamWith) {
+            foreach ($callback() as $message) {
+                if (connection_aborted()) {
+                    break;
+                }
+
+                $event = 'update';
+
+                if ($message instanceof StreamedEvent) {
+                    $event = $message->event;
+                    $message = $message->data;
+                }
+
+                if (! is_string($message) && ! is_numeric($message)) {
+                    $message = Js::encode($message);
+                }
+
+                echo "event: $event\n";
+                echo 'data: '.$message;
+                echo "\n\n";
+
+                if (ob_get_level() > 0) {
+                    ob_flush();
+                }
+
+                flush();
+            }
+
+            if (filled($endStreamWith)) {
+                $endEvent = 'update';
+
+                if ($endStreamWith instanceof StreamedEvent) {
+                    $endEvent = $endStreamWith->event;
+                    $endStreamWith = $endStreamWith->data;
+                }
+
+                echo "event: $endEvent\n";
+                echo 'data: '.$endStreamWith;
+                echo "\n\n";
+
+                if (ob_get_level() > 0) {
+                    ob_flush();
+                }
+
+                flush();
+            }
+        }, 200, array_merge($headers, [
+            'Content-Type' => 'text/event-stream',
+            'Cache-Control' => 'no-cache',
+            'X-Accel-Buffering' => 'no',
+        ]));
+    }
 
     /**
      * Create a new streamed response instance.
@@ -65,7 +190,10 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Symfony\Component\HttpFoundation\StreamedResponse
      */
-    public function stream($callback, $status = 200, array $headers = []);
+    public function stream($callback, $status = 200, array $headers = [])
+    {
+        return new StreamedResponse($callback, $status, $headers);
+    }
 
     /**
      * Create a new streamed JSON response instance.
@@ -76,7 +204,10 @@ interface ResponseFactory
      * @param  int  $encodingOptions
      * @return \Symfony\Component\HttpFoundation\StreamedJsonResponse
      */
-    public function streamJson($data, $status = 200, $headers = [], $encodingOptions = 15);
+    public function streamJson($data, $status = 200, $headers = [], $encodingOptions = JsonResponse::DEFAULT_ENCODING_OPTIONS)
+    {
+        return new StreamedJsonResponse($data, $status, $headers, $encodingOptions);
+    }
 
     /**
      * Create a new streamed response instance as a file download.
@@ -86,8 +217,31 @@ interface ResponseFactory
      * @param  array  $headers
      * @param  string|null  $disposition
      * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     *
+     * @throws \Illuminate\Routing\Exceptions\StreamedResponseException
      */
-    public function streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment');
+    public function streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment')
+    {
+        $withWrappedException = function () use ($callback) {
+            try {
+                $callback();
+            } catch (Throwable $e) {
+                throw new StreamedResponseException($e);
+            }
+        };
+
+        $response = new StreamedResponse($withWrappedException, 200, $headers);
+
+        if (! is_null($name)) {
+            $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+                $disposition,
+                $name,
+                $this->fallbackName($name)
+            ));
+        }
+
+        return $response;
+    }
 
     /**
      * Create a new file download response.
@@ -98,7 +252,27 @@ interface ResponseFactory
      * @param  string|null  $disposition
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function download($file, $name = null, array $headers = [], $disposition = 'attachment');
+    public function download($file, $name = null, array $headers = [], $disposition = 'attachment')
+    {
+        $response = new BinaryFileResponse($file, 200, $headers, true, $disposition);
+
+        if (! is_null($name)) {
+            return $response->setContentDisposition($disposition, $name, $this->fallbackName($name));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Convert the string to ASCII characters that are equivalent to the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function fallbackName($name)
+    {
+        return str_replace('%', '', Str::ascii($name));
+    }
 
     /**
      * Return the raw contents of a binary file.
@@ -107,7 +281,10 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function file($file, array $headers = []);
+    public function file($file, array $headers = [])
+    {
+        return new BinaryFileResponse($file, 200, $headers);
+    }
 
     /**
      * Create a new redirect response to the given path.
@@ -118,7 +295,10 @@ interface ResponseFactory
      * @param  bool|null  $secure
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirectTo($path, $status = 302, $headers = [], $secure = null);
+    public function redirectTo($path, $status = 302, $headers = [], $secure = null)
+    {
+        return $this->redirector->to($path, $status, $headers, $secure);
+    }
 
     /**
      * Create a new redirect response to a named route.
@@ -129,7 +309,10 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirectToRoute($route, $parameters = [], $status = 302, $headers = []);
+    public function redirectToRoute($route, $parameters = [], $status = 302, $headers = [])
+    {
+        return $this->redirector->route($route, $parameters, $status, $headers);
+    }
 
     /**
      * Create a new redirect response to a controller action.
@@ -140,7 +323,10 @@ interface ResponseFactory
      * @param  array  $headers
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirectToAction($action, $parameters = [], $status = 302, $headers = []);
+    public function redirectToAction($action, $parameters = [], $status = 302, $headers = [])
+    {
+        return $this->redirector->action($action, $parameters, $status, $headers);
+    }
 
     /**
      * Create a new redirect response, while putting the current URL in the session.
@@ -151,7 +337,10 @@ interface ResponseFactory
      * @param  bool|null  $secure
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirectGuest($path, $status = 302, $headers = [], $secure = null);
+    public function redirectGuest($path, $status = 302, $headers = [], $secure = null)
+    {
+        return $this->redirector->guest($path, $status, $headers, $secure);
+    }
 
     /**
      * Create a new redirect response to the previously intended location.
@@ -162,5 +351,8 @@ interface ResponseFactory
      * @param  bool|null  $secure
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null);
+    public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null)
+    {
+        return $this->redirector->intended($default, $status, $headers, $secure);
+    }
 }
